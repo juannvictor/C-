2bf6cfd10377e4604d56eb1cc71a1fe5d8987be7
Juan Juan <juannvictor1998@gmail.com>
2018-02-22 15:28:18 -0300

    Update Vetores.cpp

--- Vetores.cpp
+++ Vetores.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-// Declarando as funções
+// Declarando as funÃ§Ãµes
 
 void imprimeVetor(int[], int);
 int preencheAleatorio(int[], int);
@@ -15,8 +15,11 @@ int somaVetor(int[], int);
 int somaParesVetor(int[], int);
 int buscaElemVetor(int[], int, int);
 int contaOcorrenciasElemVetor(int v[], int n, int elem);
+int maiorValor(int v[], int n);
+int qtdeElemMenores(int v[], int n, int elem);
+int compararVetores(int v[], int w[], int n);
 
-// Implementação das funções
+// ImplementaÃ§Ã£o das funÃ§Ãµes
 
 int preencheAleatorio(int v[], int n){
 	int k = 0;
@@ -45,7 +48,7 @@ int somaVetor(int v[], int n){
 	return total;
 }
 
-// Exercícios: Complete esse código com funções para:
+// ExercÃ­cios: Complete esse cÃ³digo com funÃ§Ãµes para:
 //  1 - Somar os valores pares de um vetor
 
 int somaParesVetor(int v[], int n){
@@ -65,13 +68,11 @@ int buscaElemVetor(int v[], int n, int elem){
 		if (v[k] == elem){
 			return k;
 		}
-		else {
-			return -1;
-		}
 	}
+	return -1;
 }
 
-// 3 - Retornar a quantidade de ocorrências de um dado elemento em um vetor
+// 3 - Retornar a quantidade de ocorrÃªncias de um dado elemento em um vetor
 
 int contaOcorrenciasElemVetor(int v[], int n, int elem){
 	int qtd = 0;
@@ -83,25 +84,63 @@ int contaOcorrenciasElemVetor(int v[], int n, int elem){
 	return qtd;
 }
 
-// 4 - Retornar o valor do maior elemento de um vetor não vazio
-// 5 - Retornar a quantidade de elementos do vetor que são menores que um dado valor
-// 6 - Comparar se dois vetores são iguais
+// 4 - Retornar o valor do maior elemento de um vetor nÃ£o vazio
 
-//  Função main
+int maiorValor(int v[], int n){
+	int k, maiorvalor = 0;
+	for (k=0; k<n; k++){
+		if(v[k] > maiorvalor){
+			maiorvalor = v[k];
+		}
+	}
+	return maiorvalor;
+}
+// 5 - Retornar a quantidade de elementos do vetor que sÃ£o menores que um dado valor
+
+int qtdeElemMenores(int v[], int n, int elem){
+	int qtd = 0;
+	for (int k=0; k<n; k++){
+		if (v[k] < elem){
+			qtd++;
+		}
+	}
+	return qtd;
+}
+// 6 - Comparar se dois vetores sÃ£o iguais
+int compararVetores(int v[], int w[], int n){
+	int qtd = 0;
+	for (int k = 0; k<n; k++){
+		if (v[k] == w[k]){
+			qtd++;
+		}
+		else{
+			return -1;
+		}
+		return qtd;
+	}
+}
+
+//  FunÃ§Ã£o main
 
 int main(){
 	int v[TAM_MAX];
+	int w[TAM_MAX];
 	int qtdVet = 0;
 	int num;
 	int indice;
 	int ocorrencias;
+	int maiorvalor;
+	int menores;
+	int num2;
+	int compararVetor;
 	
-	imprimeVetor(v, qtdVet);
 	qtdVet = preencheAleatorio(v,5);
 	imprimeVetor(v, qtdVet);
 	
-	cout << "A soma dos elementos do vetor é: " << somaVetor(v, qtdVet) << endl;
-	cout << "A soma dos pares do vetor é " << somaParesVetor(v, qtdVet) << endl << endl;
+	cout << "A soma dos elementos do vetor e: " << somaVetor(v, qtdVet) << endl;
+	cout << "A soma dos pares do vetor e: " << somaParesVetor(v, qtdVet) << endl;
+	maiorvalor = maiorValor(v, qtdVet);
+	cout << maiorvalor << " e o maior elemento do vetor." << endl << endl;
 	
 	cout << "Digite o valor a ser buscado no vetor: ";
 	cin  >> num;
@@ -115,7 +154,26 @@ int main(){
 	}
 	
 	ocorrencias = contaOcorrenciasElemVetor(v, qtdVet, num);
-	cout << num << " ocorre " << ocorrencias << " vezes no vetor\n";
+	cout << num << " ocorre " << ocorrencias << " vezes no vetor\n" << endl;
+	
+	cout << "Digite um numero a ser comparado: ";
+	cin  >> num2;
+	menores = qtdeElemMenores(v, qtdVet, num2);
+	cout << menores << " numeros no vetor sao menores que " << num2 << endl << endl;
+	
+	qtdVet = preencheAleatorio(v,5);
+	imprimeVetor(v, qtdVet);
+	qtdVet = preencheAleatorio(w,5);
+	imprimeVetor(w, qtdVet);
+	
+	compararVetor = compararVetores(v, w, qtdVet);
+	if (compararVetor >= 0){
+		cout << "Os vetores sao iguais." << endl;
+	}
+	else{
+		cout << "Os vetores nao sao iguais." << endl;
+	}
+	
 	
 	// system("PAUSE");
 	return 0;
